eb0aecd885bccbe178d7fedcde1eb6e44b50ae2f
Matthew Hoffman <wyeast@gmail.com>
2016-01-27 16:41:48 -0700

    Update LI grid gen batch script for Mustang
    Specifically, this was used for the high res AIS mesh.

--- grid_gen/landice_grid_tools/mpas_mesh_to_landice_ic_batch.sh
+++ grid_gen/landice_grid_tools/mpas_mesh_to_landice_ic_batch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#MSUB -l walltime=02:00:00
+#MSUB -l nodes=1
+#MSUB -j oe
+
+source ~/setup_gnu_env.sh
+
 # This script is a template for a batch script that will perform the various steps required
 # to go from a base MPAS mesh to a culled landice mesh with initial conditions.
 # It's main purpose is to be used on clusters with a batch system for situations
@@ -9,38 +15,46 @@
 
 
 # Location of MPAS-Tools repo
-TOOLS=/Users/mhoffman/documents/mpas-git/Tools
+TOOLS=/users/mhoffman/mpas/MPAS-Tools
 
 # Command used on the machine for running an executable
-RUNCMD=""
+RUNCMD="mpirun -n 1 "
 
 # The name of the mesh with which processing
 STARTMESH=mpas.nc
 
 # The name of the initial condition file from which initial conditions will be interpolated
-INTERPFILE=?
+INTERPFILE=/usr/projects/climate/mhoffman/AIS_IC_data/Antarctica-1km.BISICLES.CISM-style.nc
 
 # Amount to translate the mesh in x and y
 SHIFTX=0.0
 SHIFTY=0.0
 
 # Projection to use for adding lat/lon values.  See that script for options or to add new ones
-PROJ=aid-bedmap2
+PROJ=ais-bedmap2
 
 # ==========================
 
 date
 
 # shift by some amount
-$RUNCMD $TOOLS/grid_gen/landice_grid_tools/grid_gen/planar_grid_transformations/translate_planar_grid.py -f $STARTMESH -x $SHIFTX -y $SHIFTY 
+#$RUNCMD $TOOLS/grid_gen/planar_grid_transformations/translate_planar_grid.py -f $STARTMESH -x $SHIFTX -y $SHIFTY 
+date
+
+# generate culling mask
+$RUNCMD $TOOLS/grid_gen/landice_grid_tools/define_cullMask.py -f $STARTMESH -m 4  # method 4 removed junk from near the edges
+date
+
+# Cull mesh
+$RUNCMD $TOOLS/grid_gen/mesh_conversion_tools/MpasCellCuller.x $STARTMESH culled1.nc
 date
 
 # Add lat/lon
-$RUNCMD $TOOLS/grid_gen/planar_grid_transformations/set_lat_lon_fields_in_planar_grid.py -f $STARTMESH -p $PROJ
+$RUNCMD $TOOLS/grid_gen/planar_grid_transformations/set_lat_lon_fields_in_planar_grid.py -f culled1.nc -p $PROJ
 date
 
 # convert to LI mesh - no reason to have many vertical levels yet since we are going to cull
-$RUNCMD $TOOLS/grid_gen/landice_grid_tools/create_landice_grid_from_generic_MPAS_grid.py -i $STARTMESH -o landice_grid_full.nc -l 2 --beta
+$RUNCMD $TOOLS/grid_gen/landice_grid_tools/create_landice_grid_from_generic_MPAS_grid.py -i culled1.nc -o landice_grid_full.nc -l 2 --beta
 date
 
 # Interpolate thickness
@@ -48,15 +62,15 @@ $RUNCMD $TOOLS/grid_gen/landice_grid_tools/interpolate_cism_grid_to_mpas_grid.py
 date
 
 # generate culling mask
-$RUNCMD $TOOLS/grid_gen/landice_grid_tools/define_cullMask.py -f landice_grid_full.nc
+$RUNCMD $TOOLS/grid_gen/landice_grid_tools/define_cullMask.py -f landice_grid_full.nc -m 2
 date
 
 # Cull mesh
-$RUNCMD $TOOLS/grid_gen/mesh_conversion_tools/MpasCellCuller.x landice_grid_full.nc culled.nc
+$RUNCMD $TOOLS/grid_gen/mesh_conversion_tools/MpasCellCuller.x landice_grid_full.nc culled2.nc
 date
 
 # Make LI mesh
-$RUNCMD $TOOLS/grid_gen/landice_grid_tools/create_landice_grid_from_generic_MPAS_grid.py -i culled.nc -o landice_grid.nc -l 10 --beta
+$RUNCMD $TOOLS/grid_gen/landice_grid_tools/create_landice_grid_from_generic_MPAS_grid.py -i culled2.nc -o landice_grid.nc -l 10 --beta
 date
 
 # Interpolate everything
