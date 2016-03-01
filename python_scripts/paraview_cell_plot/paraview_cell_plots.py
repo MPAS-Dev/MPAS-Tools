15a2583ee266058419db8df71449c847d84b4e8d
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2016-03-01 14:06:22 -0700

    Break script up into functions
    This commit moves different parts of the script into functions to make
    them easier to reuse / modify.

--- python_scripts/paraview_cell_plot/paraview_cell_plots.py
+++ python_scripts/paraview_cell_plot/paraview_cell_plots.py
@@ -35,9 +35,6 @@ from progressbar import *
 def setup_time_indices(fn_pattern, local_indices, global_indices, file_names):#{{{
     # Build file list and time indices
     file_list = sorted(glob.glob(fn_pattern))
-    #local_indices = []
-    #file_names = []
-    #global_indices = []
 
     widgets = ['Build time indices: ', Percentage(), ' ', Bar(), ' ', ETA()]
     time_bar = ProgressBar(widgets=widgets, maxval=len(file_list)).start()
@@ -61,34 +58,11 @@ def setup_time_indices(fn_pattern, local_indices, global_indices, file_names):#{
     time_bar.finish()
 #}}}
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-f", "--file_pattern", dest="filename_pattern", help="MPAS Filename pattern.", metavar="FILE", required=True)
-    parser.add_argument("-b", "--blocking", dest="blocking", help="Size of blocks when reading MPAS file", metavar="BLK")
-    parser.add_argument("-v", "--variable", dest="variable", help="Variable to plot.", metavar="VAR", required=True)
-
-    args = parser.parse_args()
-
-    if not args.blocking:
-        args.blocking = int(10000)
-    else:
-        args.blocking = int(args.blocking)
-
-    Points = vtk.vtkPoints()
-    Cells = vtk.vtkCellArray()
-
-    time_indices = []
-    time_file = []
-    time_global_indices = []
-    setup_time_indices(args.filename_pattern, time_indices, time_global_indices, time_file)
-
-    nc_file = NetCDFFile(time_file[0], 'r')
-
+def setup_dimension_values(field_var, dim_vals):#{{{
     # Setting dimension values:
     first_dim_set = True
-    field_dims = nc_file.variables[args.variable].dimensions
+    field_dims = field_var.dimensions
     excluded_dims = {'Time', 'nCells', 'nEdges', 'nVertices'}
-    dim_vals = []
     for dim in field_dims:
         if dim == 'Time':
             dim_vals.append(-1)
@@ -113,23 +87,22 @@ if __name__ == "__main__":
                 else:
                     print " -- Invalid value, please re-enter --"
 
-    nCells = len(nc_file.dimensions['nCells'])
+#}}}
+
+def build_vertex_lists_xyz( nc_file, blocking, point_list ):#{{{
     nVertices = len(nc_file.dimensions['nVertices'])
 
     xVertex_var = nc_file.variables['xVertex']
     yVertex_var = nc_file.variables['yVertex']
     zVertex_var = nc_file.variables['zVertex']
 
-    nEdgesOnCell_var = nc_file.variables['nEdgesOnCell']
-    verticesOnCell_var = nc_file.variables['verticesOnCell']
-
     # Build vertex list
-    nBlocks = 1 + nVertices / args.blocking
+    nBlocks = 1 + nVertices / blocking
     widgets = ['Build vertex list: ', Percentage(), ' ', Bar(), ' ', ETA()]
     vertex_bar = ProgressBar(widgets=widgets, maxval=nBlocks).start()
     for iBlock in np.arange(0, nBlocks):
-        blockStart = iBlock * args.blocking
-        blockEnd = min( (iBlock + 1) * args.blocking, nVertices )
+        blockStart = iBlock * blocking
+        blockEnd = min( (iBlock + 1) * blocking, nVertices )
         blockCount = blockEnd - blockStart
 
         #print "  On vertex block %d out of %d (vertices %d through %d out of %d, contains %d vertices)"%(iBlock, nBlocks, blockStart, blockEnd, nVertices, blockCount)
@@ -139,7 +112,7 @@ if __name__ == "__main__":
         zVertex = zVertex_var[blockStart:blockEnd]
 
         for idx in np.arange(0, blockCount):
-            id = Points.InsertNextPoint(xVertex[idx], yVertex[idx], zVertex[idx])
+            id = point_list.InsertNextPoint(xVertex[idx], yVertex[idx], zVertex[idx])
         
         del xVertex
         del yVertex
@@ -147,14 +120,21 @@ if __name__ == "__main__":
         vertex_bar.update(iBlock)
 
     vertex_bar.finish()
+#}}}
+
+def build_cell_lists( nc_file, blocking, cell_list ):#{{{
+    nCells = len(nc_file.dimensions['nCells'])
+
+    nEdgesOnCell_var = nc_file.variables['nEdgesOnCell']
+    verticesOnCell_var = nc_file.variables['verticesOnCell']
 
     # Build cells
-    nBlocks = 1 + nCells / args.blocking
+    nBlocks = 1 + nCells / blocking
     widgets = ['Build cell connectivity: ', Percentage(), ' ', Bar(), ' ', ETA()]
     cell_bar = ProgressBar(widgets=widgets, maxval=nBlocks).start()
     for iBlock in np.arange(0, nBlocks):
-        blockStart = iBlock * args.blocking
-        blockEnd = min( (iBlock + 1) * args.blocking, nCells )
+        blockStart = iBlock * blocking
+        blockEnd = min( (iBlock + 1) * blocking, nCells )
         blockCount = blockEnd - blockStart
 
         nEdgesOnCell = nEdgesOnCell_var[blockStart:blockEnd]
@@ -167,7 +147,7 @@ if __name__ == "__main__":
                 vert = verticesOnCell[idx][iVertex] - 1
                 polygon.GetPointIds().SetId(iVertex, vert)
 
-            Cells.InsertNextCell(polygon)
+            cell_list.InsertNextCell(polygon)
 
         del nEdgesOnCell
         del verticesOnCell
@@ -175,30 +155,31 @@ if __name__ == "__main__":
 
     cell_bar.finish()
 
-    nc_file.close()
+#}}}
 
+def build_field_time_series( local_indices, global_indices, file_names, var_name, point_list, cell_list ):#{{{
     polydata = vtk.vtkPolyData()
     polydata.SetPoints(Points)
     polydata.SetPolys(Cells)
 
-    if not os.path.exists('vtk_files/time_series'):
-        os.makedirs('vtk_files/time_series')
-        
     # Output time series
     widgets = ['Writing time series: ', Percentage(), ' ', Bar(), ' ', ETA()]
-    field_bar = ProgressBar(widgets=widgets, maxval=len(time_global_indices)).start()
+    field_bar = ProgressBar(widgets=widgets, maxval=len(global_indices) ).start()
     prev_file = ""
-    for time_index in time_global_indices:
+    for time_index in global_indices:
 
-        if prev_file != time_file[time_index]:
+        if prev_file != file_names[time_index]:
             if prev_file != "":
                 nc_file.close()
-            nc_file = NetCDFFile(time_file[time_index], 'r')
-            prev_file = time_file[time_index]
+            nc_file = NetCDFFile(file_names[time_index], 'r')
+            prev_file = file_names[time_index]
 
         Colors = vtk.vtkTypeFloat64Array()
         Colors.SetNumberOfComponents(1);
         Colors.SetName(args.variable);
+
+        nCells = len(nc_file.dimensions['nCells'])
+        nBlocks = 1 + nCells / args.blocking
 
         field_var = nc_file.variables[args.variable]
         field_ndims = len(field_var.dimensions)
@@ -212,13 +193,13 @@ if __name__ == "__main__":
             if field_ndims == 1:
                 field = field_var[blockStart:blockEnd]
             if field_ndims == 2:
-                field = field_var[time_indices[time_index], blockStart:blockEnd]
+                field = field_var[local_indices[time_index], blockStart:blockEnd]
             elif field_ndims == 3:
-                field = field_var[time_indices[time_index], blockStart:blockEnd, 0]
+                field = field_var[local_indices[time_index], blockStart:blockEnd, 0]
             elif field_ndims == 4:
-                field = field_var[time_indices[time_index], blockStart:blockEnd, dim_vals[2], dim_vals[3]]
+                field = field_var[local_indices[time_index], blockStart:blockEnd, dim_vals[2], dim_vals[3]]
             elif field_ndims == 5:
-                field = field_var[time_indices[time_index], blockStart:blockEnd, dim_vals[2], dim_vals[3], dim_vals[4]]
+                field = field_var[local_indices[time_index], blockStart:blockEnd, dim_vals[2], dim_vals[3], dim_vals[4]]
 
             for idx in np.arange(0, blockCount):
                 Colors.InsertNextTuple1(field[idx]);
@@ -255,5 +236,47 @@ if __name__ == "__main__":
         pvd_file.write('\tfile="time_series/%s.%d.vtp"/>\n'%(args.variable, time_index))
     pvd_file.write('</Collection>\n')
     pvd_file.write('</VTKFile>\n')
+#}}}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-f", "--file_pattern", dest="filename_pattern", help="MPAS Filename pattern.", metavar="FILE", required=True)
+    parser.add_argument("-b", "--blocking", dest="blocking", help="Size of blocks when reading MPAS file", metavar="BLK")
+    parser.add_argument("-v", "--variable", dest="variable", help="Variable to plot.", metavar="VAR", required=True)
+
+    args = parser.parse_args()
+
+    if not args.blocking:
+        args.blocking = int(10000)
+    else:
+        args.blocking = int(args.blocking)
+
+    Points = vtk.vtkPoints()
+    Cells = vtk.vtkCellArray()
+
+    time_indices = []
+    time_file = []
+    time_global_indices = []
+    setup_time_indices(args.filename_pattern, time_indices, time_global_indices, time_file)
+
+    nc_file = NetCDFFile(time_file[0], 'r')
+
+    # Setting dimension values:
+    dim_vals = []
+    field_var = nc_file.variables[args.variable]
+    setup_dimension_values( field_var, dim_vals )
+
+    # Build vertex list
+    build_vertex_lists_xyz( nc_file, args.blocking, Points )
+
+    # Build cell list
+    build_cell_lists( nc_file, args.blocking, Cells )
+
+    nc_file.close()
+
+    if not os.path.exists('vtk_files/time_series'):
+        os.makedirs('vtk_files/time_series')
+
+    build_field_time_series( time_indices, time_global_indices, time_file, args.variable, Points, Cells )
 
 # vim: set expandtab:
