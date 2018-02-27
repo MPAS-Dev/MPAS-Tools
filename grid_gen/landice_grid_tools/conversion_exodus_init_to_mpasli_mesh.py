506f161e5635f79d53068b360287e0f21682eaae
tongzhangice <zhgtong@gmail.com>
2018-02-26 18:06:02 -0700

    clean it by for example changing a specific varible name to a general method by adding new options at the beginning

--- grid_gen/landice_grid_tools/conversion_exodus_init_to_mpasli_mesh.py
+++ grid_gen/landice_grid_tools/conversion_exodus_init_to_mpasli_mesh.py
@@ -21,42 +21,30 @@ parser.add_option("-e", "--exo", dest="exo_file", help="the exo input file")
 parser.add_option("-a", "--ascii", dest="id_file", help="the ascii global id input file")
 parser.add_option("-m", "--method", dest="conversion_method", help="two options: id or coord")
 parser.add_option("-o", "--out", dest="nc_file", help="the mpas output file")
+parser.add_option("-v", "--variable", dest="var_name", help="the mpas variable you want to convert from an exodus file")
+parser.add_option("-x", "--extra", dest="extrapolation", help="set 1 if you want do extrapolation for surrounding buffer region, 0 for turning it down. The current extrapolation method is a simple average of active ice cell values. A distance inversed method is still needed for variable resolution meshes.")
 options, args = parser.parse_args()
 
+mpas_exodus_var_dic = {"beta":"basal_friction", "thickness":"ice_thickness"}
+# A mapping between mpas and exodus file. Add one if you need to manipulate a different variable!
+
+exo_var_name = mpas_exodus_var_dic[options.var_name]
 
 dataset = Dataset(options.nc_file, 'r+', format="NETCDF4")
-#beta = dataset.variables['beta'][0,:]
-#thickness = dataset.variables['thickness'][0,:]
-# read the old basal friction data in the input MPAS file
-#allCellID = dataset.variables['indexToCellID'][:]
-# read the cell ID numbers in the input MPAS file
 x = dataset.variables['xCell'][:]
 y = dataset.variables['yCell'][:]
 xy = np.array([x,y])
 tree = spt.cKDTree(xy.T)
 
-#usefullCellID = np.loadtxt(options.id_file)
-# read the targeted global id numbers from the ascii input file
-#usefullCellID = [int(i) for i in usefullCellID[1::]]
-# convert the number from float to int
-
-#model = exomerge.import_model(options.exo_file)
 exo = exodus(options.exo_file)
-#model.output_global_variables('global_variables.csv')
 
-#stride = model.global_variables['stride'][0]
 stride = np.array(exo.get_global_variable_values('stride'))
-#ordering = model.global_variables['ordering'][0]
 ordering = np.array(exo.get_global_variable_values('ordering'))
 # if ordering = 1, exo data is in the column-wise manner, stride is the vertical layer number
 # if ordering = 0, exo data is in the layer-wise manner, stride is the node number each layer
 
-#basal_friction = model.get_node_field_values("basal_friction")
-beta_exo = np.array(exo.get_node_variable_values('basal_friction',1))
-thickness_exo = np.array(exo.get_node_variable_values('ice_thickness',1))
-# read the basal_friction data from the exo file
-#ice_thickness = model.get_node_field_values("ice_thickness")
-#ice_thickness = exo.get_node_variable_values('ice_thickness',1)[:]
+data_exo = np.array(exo.get_node_variable_values(exo_var_name,1))
+# read data from the exo file
 
 xyz_exo = exo.get_coords()
 x_exo = np.array(xyz_exo[0]) * 1000
@@ -66,15 +54,14 @@ y_exo = np.array(xyz_exo[1]) * 1000
 if ordering == 1.0:
     print "column wise pattern"
     layer_num = int(stride)
-    beta_exo_layer = beta_exo[::layer_num]
-    thickness_exo_layer = thickness_exo[::layer_num]
+    data_exo_layer = data_exo[::layer_num]
+    #thickness_exo_layer = thickness_exo[::layer_num]
     x_exo_layer = x_exo[::layer_num]
     y_exo_layer = y_exo[::layer_num]
-#    ice_thickness_MPAS = ice_thickness[::layer_num]
 elif ordering == 0.0:
     print "layer wise pattern"
     node_num = int(stride)
-    beta_exo_layer = beta_exo[0:node_num+1]
+    data_exo_layer = data_exo[0:node_num+1]
     x_exo_layer = x_exo[0:node_num+1]
     y_exo_layer = y_exo[0:node_num+1]
 else:
@@ -83,10 +70,8 @@ else:
 
 node_num_layer = len(x_exo_layer)
 
-#index_array = np.zeros(node_num_layer)
-#thickness_array = np.zeros(node_num_layer)
-
-dataset.variables['beta'][0,:] = 1.0e-10
+#dataset.variables[options.var_name][0,:] = 1.0e-10
+# set beta value to some uniform value before we put new data in it
 
 if (options.conversion_method == 'coord'):
     print "use coordinate method"
@@ -96,19 +81,15 @@ if (options.conversion_method == 'coord'):
         # perhaps there is a better criteria for identifying coords?
         index_intersect = list(set(index_x) & set(index_y))
         index = index_intersect[0]
-        #index_array[i] = index
-        #thickness_array[i] = thickness_exo_layer[i] 
+        dataset.variables[options.var_name][0,index] = np.exp(data_exo_layer[i])
+        # The beta unit of the mpas mesh is kPa yr/m, not SI
 
-        #dataset.variables['beta'][0,index] = thickness_exo_layer[i]
-        dataset.variables['beta'][0,index] = np.exp(beta_exo_layer[i])
-        # The beta unit of the mpas mesh is kPa yr/m ?
 elif (options.conversion_method == 'id'):
     print "use global id method. A global id file have to supplied by prescribing the id_file"
     usefullCellID = np.loadtxt(options.id_file,dtype='i')
     usefullCellID_array = usefullCellID[1::]
     # The first number in the file is the total number. skip it
-    #dataset.variables['beta'][0,usefullCellID_array-1] = np.exp(beta_exo_layer)
-    dataset.variables['beta'][0,usefullCellID_array-1] = np.exp(beta_exo_layer)
+    dataset.variables[options.var_name][0,usefullCellID_array-1] = np.exp(data_exo_layer)
     # We need convert fortran indice to python indice
 
 else:
@@ -125,8 +106,9 @@ keepCellMask = np.copy(cullCell[:])
 keepCellMask[:] = 0
 keepCellMask[thickness > 0.0] = 1
 
-
 keepCellMaskNew = np.copy(keepCellMask)  # make a copy to edit that can be edited without changing the original
+
+# The following section is to mask the buffer region. We do not use it in this script
 
 #for iCell in range(len(nEdgesOnCell)):
 #    if keepCellMask[iCell] == 0:
@@ -135,10 +117,8 @@ keepCellMaskNew = np.copy(keepCellMask)  # make a copy to edit that can be edite
 #                keepCellMaskNew[iCell] = 1
 #
 #
-#dataset.variables['beta'][0,:]=keepCellMaskNew
+#dataset.variables[options.var_name][0,:]=keepCellMaskNew
 
-
-#cellsOnCell_new = np.copy(cellsOnCell)
 
 # recursive extrapolation steps:
 # 1) find cell A with mask = 0 (ice_thickness = 0)
@@ -149,51 +129,55 @@ keepCellMaskNew = np.copy(keepCellMask)  # make a copy to edit that can be edite
 # 6) Update mask
 # 7) go to step 1)
 
-while np.count_nonzero(keepCellMask) != nCells:
+if options.extrapolation == "1":
+
+    while np.count_nonzero(keepCellMask) != nCells:
 
 
-    if 0:
-        for iCell in range(nCells):
-            if keepCellMask[iCell] == 0:
-                x_tmp = x[iCell]
-                y_tmp = y[iCell]
-                neareastPointNum = nEdgesOnCell[iCell]+1
-                dist, idx = tree.query([x_tmp,y_tmp],k=neareastPointNum) # KD tree take account of [x_tmp,y_tmp] itself
-                mask_for_idx = keepCellMask[idx]
-                mask_nonzero_idx, = np.nonzero(mask_for_idx)
+        if 0:
+            for iCell in range(nCells):
+                if keepCellMask[iCell] == 0:
+                    x_tmp = x[iCell]
+                    y_tmp = y[iCell]
+                    neareastPointNum = nEdgesOnCell[iCell]+1
+                    dist, idx = tree.query([x_tmp,y_tmp],k=neareastPointNum) # KD tree take account of [x_tmp,y_tmp] itself
+                    mask_for_idx = keepCellMask[idx]
+                    mask_nonzero_idx, = np.nonzero(mask_for_idx)
 
-                nonzero_id = idx[mask_nonzero_idx]
-                nonzero_num = np.count_nonzero(mask_for_idx)
-
-                if nonzero_num > 0:
-                    dataset.variables['beta'][0,iCell] = sum(dataset.variables['beta'][0,nonzero_id])/nonzero_num
-                    keepCellMaskNew[iCell] = 1;
-
-        keepCellMask = np.copy(keepCellMaskNew)
-        print "%d cells left for extrapolation!" % (nCells-np.count_nonzero(keepCellMask))
-
-    else:
-        for iCell in range(nCells):
-            if keepCellMask[iCell] == 0:
-                neighborCellId = cellsOnCell[iCell,:nEdgesOnCell[iCell]]-1
-                nonzero_idx, = np.nonzero(neighborCellId+1)
-                cell_nonzero_id = neighborCellId[nonzero_idx] # neighbor cell id
-
-                mask_for_idx = keepCellMask[cell_nonzero_id] # active cell mask
-                mask_nonzero_idx, = np.nonzero(mask_for_idx)
-
-                nonzero_id = cell_nonzero_id[mask_nonzero_idx] # id for nonzero beta cells
-                nonzero_num = np.count_nonzero(mask_for_idx)
-
-                if len(nonzero_id) == nonzero_num:
+                    nonzero_id = idx[mask_nonzero_idx]
+                    nonzero_num = np.count_nonzero(mask_for_idx)
 
                     if nonzero_num > 0:
-                        dataset.variables['beta'][0,iCell] = sum(dataset.variables['beta'][0,nonzero_id])/nonzero_num
+                        dataset.variables[options.var_name][0,iCell] = sum(dataset.variables[options.var_name][0,nonzero_id])/nonzero_num
                         keepCellMaskNew[iCell] = 1;
 
-        keepCellMask = np.copy(keepCellMaskNew)
-        print "%d cells left for extrapolation!" % (nCells-np.count_nonzero(keepCellMask))
+            keepCellMask = np.copy(keepCellMaskNew)
+            print "%d cells left for extrapolation!" % (nCells-np.count_nonzero(keepCellMask))
 
+            # The above section use KD Tree to find the surrounding nearest cells, thus only works for uniform meshes. But it's also useful for testing
+            # Normally, always use the method in below
+
+        else:
+            for iCell in range(nCells):
+                if keepCellMask[iCell] == 0:
+                    neighborCellId = cellsOnCell[iCell,:nEdgesOnCell[iCell]]-1
+                    nonzero_idx, = np.nonzero(neighborCellId+1)
+                    cell_nonzero_id = neighborCellId[nonzero_idx] # neighbor cell id
+
+                    mask_for_idx = keepCellMask[cell_nonzero_id] # active cell mask
+                    mask_nonzero_idx, = np.nonzero(mask_for_idx)
+
+                    nonzero_id = cell_nonzero_id[mask_nonzero_idx] # id for nonzero beta cells
+                    nonzero_num = np.count_nonzero(mask_for_idx)
+
+                    if len(nonzero_id) == nonzero_num:
+
+                        if nonzero_num > 0:
+                            dataset.variables[options.var_name][0,iCell] = sum(dataset.variables[options.var_name][0,nonzero_id])/nonzero_num
+                            keepCellMaskNew[iCell] = 1;
+
+            keepCellMask = np.copy(keepCellMaskNew)
+            print "%d cells left for extrapolation!" % (nCells-np.count_nonzero(keepCellMask))
 
 
 dataset.close()
