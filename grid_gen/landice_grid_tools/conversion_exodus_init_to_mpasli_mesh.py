315a1b9c577d04853dd8949015c6f494f4e4dc48
tongzhangice <zhgtong@gmail.com>
2018-03-02 10:29:04 -0700

    add two new lines for converting 2d exodus data to 3d layered mpas data

--- grid_gen/landice_grid_tools/conversion_exodus_init_to_mpasli_mesh.py
+++ grid_gen/landice_grid_tools/conversion_exodus_init_to_mpasli_mesh.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 """
 Created on Tue Feb 13 23:50:20 2018
 
-@author: tong
+@author: Tong Zhang
 """
 
 import sys
 sys.path.append('/home/tzhang/Apps/seacas/lib')
-#sys.path.append('/home/tzhang/Apps/exomerge')
 from exodus import exodus
-#import exomerge
 import numpy as np
 from netCDF4 import Dataset
 from optparse import OptionParser
 import scipy.spatial as spt
 
-parser = OptionParser(epilog='read the basal friction data in the exo file and put them back to MPAS mesh')
+parser = OptionParser(description='Read the basal friction data in the exo file and put them back to MPAS mesh. Change the SEACAS library dir to your own path!')
 parser.add_option("-e", "--exo", dest="exo_file", help="the exo input file")
 parser.add_option("-a", "--ascii", dest="id_file", help="the ascii global id input file")
-parser.add_option("-m", "--method", dest="conversion_method", help="two options: id or coord")
+parser.add_option("-m", "--method", dest="conversion_method", help="two options: id or coord. The id method is recommended. The coord method may fail at points where x or y = 0 while x_exodus or y_exodus is not")
 parser.add_option("-o", "--out", dest="nc_file", help="the mpas output file")
 parser.add_option("-v", "--variable", dest="var_name", help="the mpas variable you want to convert from an exodus file")
 parser.add_option("-x", "--extra", dest="extrapolation", help="set 1 if you want do extrapolation for surrounding buffer region, 0 for turning it down. The current extrapolation method is a simple average of active ice cell values. A distance inversed method is still needed for variable resolution meshes.")
 options, args = parser.parse_args()
 
-mpas_exodus_var_dic = {"beta":"basal_friction", "thickness":"ice_thickness"}
+mpas_exodus_var_dic = {"beta":"basal_friction", "thickness":"ice_thickness", "enhance:enhancement"}
 # A mapping between mpas and exodus file. Add one if you need to manipulate a different variable!
 
 exo_var_name = mpas_exodus_var_dic[options.var_name]
@@ -64,6 +61,7 @@ elif ordering == 0.0:
     data_exo_layer = data_exo[0:node_num+1]
     x_exo_layer = x_exo[0:node_num+1]
     y_exo_layer = y_exo[0:node_num+1]
+    layer_num = int(len(data_exo)/node_num)
 else:
     print "The ordering is probably wrong"
 # slice the exo data to get the MPAS data
@@ -76,49 +74,56 @@ node_num_layer = len(x_exo_layer)
 if (options.conversion_method == 'coord'):
     print "use coordinate method"
     for i in range(node_num_layer):
-        index_x, = np.where(abs(x[:]-x_exo_layer[i])<1e2)
-        index_y, = np.where(abs(y[:]-y_exo_layer[i])<1e2)
-        # perhaps there is a better criteria for identifying coords?
+        index_x, = np.where(abs(x[:]-x_exo_layer[i])/(abs(x[:])+1e-10)<1e-3)
+        index_y, = np.where(abs(y[:]-y_exo_layer[i])/(abs(y[:])+1e-10)<1e-3)
         index_intersect = list(set(index_x) & set(index_y))
         index = index_intersect[0]
-        dataset.variables[options.var_name][0,index] = np.exp(data_exo_layer[i]) * 1000.0
-        # The beta unit of the mpas mesh is Pa yr/m, not SI
+        if options.var_name == "beta":
+            dataset.variables[options.var_name][0,index] = np.exp(data_exo_layer[i]) * 1000.0
+        elif options.var_name == "enhance":
+            for l in range(layer_num):
+                dataset.variables[options.var_name][0,index,l] = data_exo_layer[i]
+        else:
+            dataset.variables[options.var_name][0,index] = (data_exo_layer[i])
+
+        # The beta unit of the mpas mesh is Pa yr/m, not SI. 
+        # This method may fail at the point where x or y = 0, while x_exo or y_exo is not
 
 elif (options.conversion_method == 'id'):
     print "use global id method. Need a global id file"
     usefullCellID = np.loadtxt(options.id_file,dtype='i')
     usefullCellID_array = usefullCellID[1::]
     # The first number in the file is the total number. skip it
-    dataset.variables[options.var_name][0,usefullCellID_array-1] = np.exp(data_exo_layer) * 1000.0
-    # We need convert fortran indice to python indice
+
+    if options.var_name == "beta":
+        dataset.variables[options.var_name][0,usefullCellID_array-1] = np.exp(data_exo_layer) * 1000.0
+    elif options.var_name == "enhance"
+        for l in range(layer_num):
+            dataset.variables[options.var_name][0,usefullCellID_array-1,l] = data_exo_layer
+    else:
+        dataset.variables[options.var_name][0,usefullCellID_array-1] = (data_exo_layer)
+        # We need convert fortran indice to python indice
 
 else:
-    print "wrong conversion method! Use id or coord only!"
+    sys.exit("wrong conversion method! Use id or coord only!")
 
 
 nCells = len(dataset.dimensions['nCells'])
-cullCell = np.zeros((nCells, ), dtype=np.int8)  # Initialize to cull no cells
 thickness = dataset.variables['thickness'][0,:]
+bedrock = dataset.variables['bedTopography'][0,:]
 cellsOnCell = dataset.variables['cellsOnCell'][:]
 nEdgesOnCell = dataset.variables['nEdgesOnCell'][:]
 
-keepCellMask = np.copy(cullCell[:])
+ice_density = 910.0
+ocean_density = 1028.0
+
+keepCellMask = np.zeros((nCells,), dtype=np.int8)
 keepCellMask[:] = 0
-keepCellMask[thickness > 0.0] = 1
+keepCellMask[thickness*ice_density/ocean_density + bedrock > 0.0] = 1
+# find the mask for grounded ice region
+#keepCellMask[thickness > 0.0] = 1
 
 keepCellMaskNew = np.copy(keepCellMask)  # make a copy to edit that can be edited without changing the original
-
-# The following section is to mask the buffer region. We do not use it in this script
-
-#for iCell in range(len(nEdgesOnCell)):
-#    if keepCellMask[iCell] == 0:
-#        for neighbor in cellsOnCell[iCell,:nEdgesOnCell[iCell]]-1:  # the -1 converts from the fortran indexing in the variable to python indexing
-#            if neighbor >= 0 and keepCellMask[neighbor] == 1:  # if any neighbors are already being kept on the old mask then keep this cell too.  This will get a lot of the ice cells, but they have already been assigned so they don't matter.  What we care about is getting cells that are not ice that have one or more ice neighbors.
-#                keepCellMaskNew[iCell] = 1
-#
-#
-#dataset.variables[options.var_name][0,:]=keepCellMaskNew
-
 
 # recursive extrapolation steps:
 # 1) find cell A with mask = 0 (ice_thickness = 0)
@@ -133,7 +138,8 @@ if options.extrapolation == "1":
 
     while np.count_nonzero(keepCellMask) != nCells:
 
-
+        # The following section use KD Tree to find the surrounding nearest cells. Thus it only works for uniform meshes. But it's still useful for testing
+        # Normally, always use the method in the else section. If you do want to use this method, change "if 0" below to "if 1"
         if 0:
             for iCell in range(nCells):
                 if keepCellMask[iCell] == 0:
@@ -153,9 +159,6 @@ if options.extrapolation == "1":
 
             keepCellMask = np.copy(keepCellMaskNew)
             print "%d cells left for extrapolation!" % (nCells-np.count_nonzero(keepCellMask))
-
-            # The above section use KD Tree to find the surrounding nearest cells, thus only works for uniform meshes. But it's also useful for testing
-            # Normally, always use the method in below
 
         else:
             for iCell in range(nCells):
