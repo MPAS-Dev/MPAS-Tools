8f873e78e8a179b6830e7b8c152c0881554d1f9f
Matthew Hoffman <mhoffman@lanl.gov>
2018-04-04 16:47:40 -0600

    Minor fixes to interp script
    * conversion of indices to integers in bilinear interp (older versions
    of numpy must have supported implicit typecasting)
    * add warning about when the delaunay method could fail due to
    limitation/bug in scipy.spatial/qhull

--- grid_gen/landice_grid_tools/interpolate_to_mpasli_grid.py
+++ grid_gen/landice_grid_tools/interpolate_to_mpasli_grid.py
@@ -99,17 +99,17 @@ def BilinearInterp(Value, gridType):
     dy = y[1] - y[0]
     for i in range(len(xCell)):
        # Calculate the CISM grid cell indices (these are the lower index)
-       xgrid = math.floor( (xCell[i]-x[0]) / dx )
+       xgrid = int(math.floor( (xCell[i]-x[0]) / dx ))
        if xgrid >= len(x) - 1:
           xgrid = len(x) - 2
        elif xgrid < 0:
           xgrid = 0
-       ygrid = math.floor( (yCell[i]-y[0]) / dy )
+       ygrid = int(math.floor( (yCell[i]-y[0]) / dy ))
        if ygrid >= len(y) - 1:
           ygrid = len(y) - 2
        elif ygrid < 0:
           ygrid = 0
-       #print xgrid, ygrid
+       #print xgrid, ygrid, i
        ValueCell[i] = Value[ygrid,xgrid] * (x[xgrid+1] - xCell[i]) * (y[ygrid+1] - yCell[i]) / (dx * dy) + \
                  Value[ygrid+1,xgrid] * (x[xgrid+1] - xCell[i]) * (yCell[i] - y[ygrid]) / (dx * dy) + \
                  Value[ygrid,xgrid+1] * (xCell[i] - x[xgrid]) * (y[ygrid+1] - yCell[i]) / (dx * dy) + \
@@ -123,7 +123,12 @@ def delaunay_interp_weights(xy, uv, d=2):
     xy = input x,y coords
     uv = output (MPSALI) x,y coords
     '''
-    tri = scipy.spatial.qhull.Delaunay(xy)
+
+    #print "scipy version=", scipy.version.full_version
+    if xy.shape[0] > 2**24-1:
+       print "WARNING: The source file contains more than 2^24-1 (16,777,215) points due to a limitation in older versions of Qhull (see: https://mail.scipy.org/pipermail/scipy-user/2015-June/036598.html).  Delaunay creation may fail if Qhull being linked by scipy.spatial is older than v2015.0.1 2015/8/31."
+
+    tri = scipy.spatial.Delaunay(xy)
     print "    Delaunay triangulation complete."
     simplex = tri.find_simplex(uv)
     print "    find_simplex complete."
