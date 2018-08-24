1df1590542d74f0fc04e9bc968c7dc702dad9e71
Mark Petersen <mpetersen@lanl.gov>
2018-08-24 08:12:07 -0600

    Add machines. Small alterations in arguments.

--- ocean/performance_testing/performance_testing.py
+++ ocean/performance_testing/performance_testing.py
@@ -9,7 +9,8 @@ This script is automatically called by call_to_performance_testing.py to run a b
 
 This script can also be used for an interactive job submission using the following command format:
 
-command format (to run an interactive job) : python performance_testing.py <Maximum Tasks> <Minimum Tasks> <cpu-type> <cores_per_node>
+command format (to run an interactive job) :
+./performance_testing.py <Maximum Tasks> <Minimum Tasks> <machine_long_name> <cores_per_node> <resolution_name>
 
 Access files required to run this script:
  1. namelist.ocean
@@ -18,9 +19,7 @@ Access files required to run this script:
  4. ocean_model (executable file)
 
 NOTE: When running a large number of tasks (>10k), check the name of log.ocean.0000.out file generated - no. of zeros in the file name changes
-
 """
-
 
 import subprocess
 import numpy as np
@@ -41,8 +40,7 @@ os.environ['OMP_PLACES'] = 'threads'
 
 timenow = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# To obtain the run duration used in calculating SYPD
-
+# Read namelist to obtain the run duration used in calculating SYPD
 time_fr = open("namelist.ocean", 'r')
 
 for line in time_fr:
@@ -61,7 +59,10 @@ for line in time_fr:
 
 time_fr.close()
 
-# To store the details on number of cells (~ resolution)
+subprocess.check_call(['mkdir', '-p', 'data_performance'])
+subprocess.check_call(['mkdir', '-p', 'figures_performance'])
+
+# Store the details on number of cells for this resolution
 
 cells_fr = open("graph.info", 'r')
 cells = str(cells_fr.readline().split(" ")[0])
@@ -69,35 +70,40 @@ cells_fr.close()
 
 nprocs_max = int(sys.argv[1])
 nprocs_min = int(sys.argv[2])
-cpu_type = sys.argv[3]
+machine_long_name = sys.argv[3]
 cores_per_node = float(sys.argv[4])
+resolution_name = sys.argv[5]
 
 # plane_size is used to define the plane_distribution flag in srun
 plane_size = str(int(cores_per_node))
 
-# Performance data evaluation begins here -
-
+# Performance data evaluation begins here
 niter = int(np.log2(nprocs_max)) - int(np.log2(nprocs_min)) + 1
-nsamples_per_procnum = 5
+nsamples_per_procnum = 3
 
 time = np.zeros(shape=(1, niter))
 procs = np.zeros(shape=(1, niter))
 SYPD = np.zeros(shape=(1, niter))
 
 i = nprocs_max
-j = niter
+j = niter - 1
 
-writefilename = "data_" + cpu_type + "_" + \
+writefilename = "data_performance/" + machine_long_name + "_" + \
     str(nprocs_max) + "_" + timenow + ".txt"
 fw = open(writefilename, 'a+')
 fw.write(
-    'Time: %s \nMachine: %s\nNo. of Cells: %s\nRun duration: %s\nRun time in sec: %d\nFormat: #Procs|Sample Runs|Average|SYPD \n' %
-    (timenow, cpu_type, cells, timeparts, simulated_time_in_sec))
+    'Time: %s \nMachine: %s\nResolution: %s\nHorizontal cells: %s\nRun duration: %s\nRun time in sec: %d\nFormat: #Procs|Sample Runs|Average|SYPD \n' %
+    (timenow,
+     machine_long_name,
+     resolution_name,
+     cells,
+     timeparts,
+     simulated_time_in_sec))
+fw.flush()
 
 while i >= nprocs_min:
 
     local_N = int(np.ceil(i / cores_per_node))
-    sample = nsamples_per_procnum
     foldername = "perf_p" + str(i) + "_gr_openmpi"
     subprocess.check_call(['rm', '-rf', foldername])
     subprocess.check_call(['mkdir', foldername])
@@ -108,7 +114,9 @@ while i >= nprocs_min:
     subprocess.check_call(['./metis', 'graph.info', str(i)])
     print "metis" + str(i) + "completed"
 
-    while sample >= 1:
+    for sample in range(nsamples_per_procnum):
+        subprocess.check_call(
+            ['rm', '-rf', 'log*', 'analysis_members', 'output.nc'])
         args = ['srun',
                 '-N',
                 str(local_N),
@@ -117,7 +125,7 @@ while i >= nprocs_min:
                 '--cpu_bind=verbose,core',
                 '--distribution=plane=%s' % plane_size,
                 './ocean_model']
-        print "running", ''.join(args)
+        print "running", ' '.join(args)
         subprocess.check_call(args)
 
         # Search for time integration and write to a file
@@ -129,40 +137,34 @@ while i >= nprocs_min:
                 first_number = numbers.split()[0]
                 fw.write('%s \t' % first_number)
                 sum = sum + float(first_number)
-
-        fname = "log_p" + str(i) + "_s" + str(sample)
+        fr.close()
+        fname = "log_p" + str(i) + "_s" + str(sample + 1)
         filepath = foldername + "/" + fname
-        sample = sample - 1
         subprocess.check_call(['mv', 'log.ocean.0000.out', filepath])
 
     average = sum / nsamples_per_procnum
-    time[0][j - 1] = average
-    procs[0][j - 1] = i
-    SYPD[0][j - 1] = simulated_time_in_sec / (365 * average)
-    fw.write('%s \t %s\n' % (str(average), str(SYPD[0][j - 1])))
+    time[0][j] = average
+    procs[0][j] = i
+    SYPD[0][j] = simulated_time_in_sec / (365 * average)
+    fw.write('%s \t %s\n' % (str(average), str(SYPD[0][j])))
+    fw.flush()
+    perfect = SYPD[0][j] / procs[0][j] * procs
+
+    # create plot with data so far
+    plt.clf()
+    plt.loglog(procs[0][j:], SYPD[0][j:], '-or',
+               label=resolution_name + ', ' + machine_long_name)
+    plt.loglog(procs[0][j:], perfect[0][j:], '--k', label='perfect scaling')
+    plt.title('MPAS-Ocean Performance Curve')
+    plt.xlabel('Number of MPI ranks')
+    plt.ylabel('Simulated Years Per Day (SYPD)')
+    plt.legend(loc='upper left')
+    plt.grid(which='major')
+    plt.xlim((procs[0][j] / 2.0, nprocs_max * 2.0))
+    plt.tight_layout()
+    figurenamepath = "figures_performance/" + resolution_name + '_' + \
+        machine_long_name + '_' + str(nprocs_max) + "_" + timenow + ".png"
+    plt.savefig(figurenamepath)
     i = i / 2
     j = j - 1
-
-# plotting ..
-
-subprocess.check_call(['mkdir', '-p', 'data_figures'])
-
-perfect = SYPD[0][0] / procs[0][0] * procs
-plt.loglog(procs[0], SYPD[0], '-or', label='grizzly')
-plt.loglog(procs[0], perfect[0], '--k', label='perfect scaling')
-plt.title(r'MPAS-Ocean Performance Curve (Broadwell 36-cores No HT)')
-plt.xlabel('Number of MPI ranks')
-plt.ylabel('Simulated Years Per Day (SYPD)')
-plt.legend(loc='upper left')
-plt.grid()
-plt.xlim((1, nprocs_max * 2))
-plt.tight_layout()
-figurenamepath = "data_figures/fig_" + cpu_type + \
-    str(nprocs_max) + "_" + timenow + ".png"
-plt.savefig(figurenamepath)
-subprocess.check_call(['mv', writefilename, 'data_figures'])
-
-fr.close()
 fw.close()
-
-# End
