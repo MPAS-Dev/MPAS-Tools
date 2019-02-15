93655c5dfa9a83e7eae4a1eaaf9a3e06af3c0edb
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-02-15 15:28:03 -0700

    Update build to use local netcdf-cxx-4.2

--- mesh_tools/mesh_conversion_tools/conda/build.sh
+++ mesh_tools/mesh_conversion_tools/conda/build.sh
@@ -3,6 +3,15 @@
 set -x
 set -e
 
-export NETCDF=$NETCDF_DIR
 cd mesh_tools/mesh_conversion_tools
+
+export CC=${GXX}
+export CFLAGS="-O3 -std=c++0x -fopenmp -lstdc++"
+
 make
+
+install -d ${PREFIX}/bin/
+for exec in MpasMeshConverter.x MpasCellCuller.x MpasMaskCreator.x mark_horns_for_culling.py
+do
+  install -m 755 ${exec} ${PREFIX}/bin/
+done
