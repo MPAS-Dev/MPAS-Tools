4494c5150f0ada45e802d5af04f2830a9643dc45
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-03-06 21:21:41 -0700

    Fix PEP8 formatting of coastline alteration scripts

--- ocean/coastline_alteration/add_land_locked_cells_to_mask.py
+++ ocean/coastline_alteration/add_land_locked_cells_to_mask.py
@@ -15,11 +15,13 @@ from netCDF4 import Dataset
 import numpy as np
 import argparse
 
+
 def removeFile(fileName):
     try:
         os.remove(fileName)
     except OSError:
         pass
+
 
 parser = \
     argparse.ArgumentParser(description=__doc__,
@@ -34,14 +36,16 @@ parser.add_argument("-m", "--mesh_file", dest="mesh_filename",
                     help="MPAS Mesh filename.", metavar="MESHFILE",
                     required=True)
 parser.add_argument("-l", "--latitude_threshold", dest="latitude_threshold",
-                    help="Minimum latitude, in degrees, for transect widening.",
+                    help="Minimum latitude, in degrees, for transect "
+                         "widening.",
                     required=False, type=float, default=43.0)
 parser.add_argument("-n", "--number_sweeps", dest="nSweeps",
-                    help="Maximum number of sweeps to search for land-locked cells.",
+                    help="Maximum number of sweeps to search for land-locked "
+                         "cells.",
                     required=False, type=int, default=10)
 args = parser.parse_args()
 
-latitude_threshold_radians = args.latitude_threshold*3.1415/180.
+latitude_threshold_radians = args.latitude_threshold * 3.1415 / 180.
 
 # Obtain mesh variables
 meshFile = Dataset(args.mesh_filename, "r")
@@ -54,7 +58,7 @@ lonCell = meshFile.variables["lonCell"][:]
 meshFile.close()
 
 removeFile(args.output_mask_filename)
-shutil.copyfile(args.input_mask_filename,args.output_mask_filename)
+shutil.copyfile(args.input_mask_filename, args.output_mask_filename)
 
 # Obtain original cell mask from input file
 inputMaskFile = Dataset(args.input_mask_filename, "r")
@@ -66,7 +70,8 @@ inputMaskFile.close()
 
 # Open output file
 outputMaskFile = Dataset(args.output_mask_filename, "a")
-landMaskDiagnostic = outputMaskFile.createVariable("landMaskDiagnostic", "i", dimensions=("nCells"))
+landMaskDiagnostic = outputMaskFile.createVariable(
+    "landMaskDiagnostic", "i", dimensions=("nCells"))
 
 print("Running add_land_locked_cells_to_mask.py.  Total number of cells: "
       "{}".format(nCells))
@@ -85,7 +90,8 @@ landLockedCounter = 0
 for iCell in range(nCells):
     landMaskDiagnostic[iCell] = landMask[iCell]
     # skip if outside latitude threshold or if this is already a land cell
-    if abs(latCell[iCell]) < latitude_threshold_radians or landMask[iCell] == 1:
+    if abs(latCell[iCell]) < latitude_threshold_radians or \
+            landMask[iCell] == 1:
         continue
     removableCellIndex[nRemovableCells] = iCell
     nRemovableCells += 1
@@ -93,7 +99,7 @@ for iCell in range(nCells):
     for iEdgeOnCell in range(nEdgesOnCell[iCell]):
         # check if neighbor is an ocean cell (landMask=0)
         # subtract 1 to convert 1-base to 0-base:
-        if landMask[cellsOnCell[iCell, iEdgeOnCell]-1] == 0:
+        if landMask[cellsOnCell[iCell, iEdgeOnCell] - 1] == 0:
             activeEdgeSum[iEdgeOnCell] += 1
             # % is modulo operator:
             iP1 = (iEdgeOnCell + 1) % nEdgesOnCell[iCell]
@@ -119,15 +125,15 @@ for iSweep in range(args.nSweeps):
         for iEdgeOnCell in range(nEdgesOnCell[iCell]):
             # check if neighbor is an ocean cell (landMask=0)
             # subtract 1 to convert 1-base to 0-base:
-            if landMask[cellsOnCell[iCell, iEdgeOnCell]-1] == 0:
+            if landMask[cellsOnCell[iCell, iEdgeOnCell] - 1] == 0:
                 # % is modulo operator:
                 iP1 = (iEdgeOnCell + 1) % nEdgesOnCell[iCell]
                 iM1 = (iEdgeOnCell - 1) % nEdgesOnCell[iCell]
                 # Is this neighbor's two neighbors to left and right land?
                 # if so, sum of masks is two.
                 # subtract 1 to convert 1-base to 0-base:
-                if (landMask[cellsOnCell[iCell, iP1]-1]
-                       + landMask[cellsOnCell[iCell, iM1]-1]) == 2:
+                if (landMask[cellsOnCell[iCell, iP1] - 1]
+                        + landMask[cellsOnCell[iCell, iM1] - 1]) == 2:
                     landLockedCounter += 1
                     landMaskNew[iCell] = 1
                     outputMaskFile['regionCellMasks'][iCell, 1] = 1
@@ -137,7 +143,7 @@ for iSweep in range(args.nSweeps):
 
     landMask[:] = landMaskNew[:]
     print("  Sweep: {} Number of landLocked cells removed: {}".format(
-        iSweep+1, landLockedCounter))
+        iSweep + 1, landLockedCounter))
     if landLockedCounter == 0:
         break
 
@@ -146,22 +152,53 @@ floodFill = np.zeros(nCells, dtype="i")
 floodableCellIndex = np.zeros(nCells, dtype="i")
 nFloodableCells = 0
 floodFill[:] = -1
-d2r = 3.1415/180.0
+d2r = 3.1415 / 180.0
 
 # init flood fill to 0 for water, -1 for land, 1 for known open ocean regions
 for iRemovableCell in range(0, nRemovableCells):
     iCell = removableCellIndex[iRemovableCell]
     if (landMaskDiagnostic[iCell] == 0):
         floodFill[iCell] = 0
-        if (latCell[iCell] > 84.0*d2r  # North Pole
-            or lonCell[iCell] > 160.0*d2r and lonCell[iCell] < 230.0*d2r and latCell[iCell] >  73.0*d2r   # Arctic
-            or lonCell[iCell] > 315.0*d2r and lonCell[iCell] < 340.0*d2r and latCell[iCell] >  15.0*d2r and latCell[iCell] <  45.0*d2r  # North Atlantic
-            or lonCell[iCell] > 290.0*d2r and lonCell[iCell] < 300.0*d2r and latCell[iCell] >  72.0*d2r and latCell[iCell] <  75.0*d2r  # North Atlantic
-            or lonCell[iCell] >   0.0*d2r and lonCell[iCell] <  10.0*d2r and latCell[iCell] >  70.0*d2r and latCell[iCell] <  75.0*d2r  # North Atlantic 2
-            or lonCell[iCell] > 150.0*d2r and lonCell[iCell] < 225.0*d2r and latCell[iCell] >   0.0*d2r and latCell[iCell] <  45.0*d2r  # North Pacific
-            or lonCell[iCell] >   0.0*d2r and lonCell[iCell] <   5.0*d2r and latCell[iCell] > -60.0*d2r and latCell[iCell] <   0.0*d2r  # South Atlantic
-            or lonCell[iCell] > 180.0*d2r and lonCell[iCell] < 280.0*d2r and latCell[iCell] > -60.0*d2r and latCell[iCell] < -10.0*d2r  # South Pacific
-            or lonCell[iCell] >   0.0*d2r and lonCell[iCell] < 165.0*d2r and latCell[iCell] > -60.0*d2r and latCell[iCell] < -45.0*d2r):  # Southern Ocean
+        if (latCell[iCell] > 84.0 * d2r  # North Pole
+                # Arctic
+                or lonCell[iCell] > 160.0 * d2r
+                and lonCell[iCell] < 230.0 * d2r
+                and latCell[iCell] > 73.0 * d2r
+                # North Atlantic
+                or lonCell[iCell] > 315.0 * d2r
+                and lonCell[iCell] < 340.0 * d2r
+                and latCell[iCell] > 15.0 * d2r
+                and latCell[iCell] < 45.0 * d2r
+                # North Atlantic
+                or lonCell[iCell] > 290.0 * d2r
+                and lonCell[iCell] < 300.0 * d2r
+                and latCell[iCell] > 72.0 * d2r
+                and latCell[iCell] < 75.0 * d2r
+                # North Atlantic 2
+                or lonCell[iCell] > 0.0 * d2r
+                and lonCell[iCell] < 10.0 * d2r
+                and latCell[iCell] > 70.0 * d2r
+                and latCell[iCell] < 75.0 * d2r
+                # North Pacific
+                or lonCell[iCell] > 150.0 * d2r
+                and lonCell[iCell] < 225.0 * d2r
+                and latCell[iCell] > 0.0 * d2r
+                and latCell[iCell] < 45.0 * d2r
+                # South Atlantic
+                or lonCell[iCell] > 0.0 * d2r
+                and lonCell[iCell] < 5.0 * d2r
+                and latCell[iCell] > -60.0 * d2r
+                and latCell[iCell] < 0.0 * d2r
+                # South Pacific
+                or lonCell[iCell] > 180.0 * d2r
+                and lonCell[iCell] < 280.0 * d2r
+                and latCell[iCell] > -60.0 * d2r
+                and latCell[iCell] < -10.0 * d2r
+                # Southern Ocean
+                or lonCell[iCell] > 0.0 * d2r
+                and lonCell[iCell] < 165.0 * d2r
+                and latCell[iCell] > -60.0 * d2r
+                and latCell[iCell] < -45.0 * d2r):
             floodFill[iCell] = 1
             landMaskDiagnostic[iCell] = 5  # indicates seed region
         else:
@@ -178,7 +215,7 @@ for iSweep in range(0, nCells):
         if (floodFill[iCell] == 0):
 
             for iCellOnCellSweep in range(0, nEdgesOnCell[iCell]):
-                iCellNeighbor = cellsOnCell[iCell, iCellOnCellSweep]-1
+                iCellNeighbor = cellsOnCell[iCell, iCellOnCellSweep] - 1
 
                 if (floodFill[iCellNeighbor] == 1):
                     floodFill[iCell] = 1
@@ -207,26 +244,28 @@ for iSweep in range(args.nSweeps):
             for iEdgeOnCell in range(nEdgesOnCell[iCell]):
                 # check if neighbor is an ocean cell (landMask=0)
                 # subtract 1 to convert 1-base to 0-base:
-                if oceanMask[cellsOnCell[iCell, iEdgeOnCell]-1] == 1:
+                if oceanMask[cellsOnCell[iCell, iEdgeOnCell] - 1] == 1:
                     # % is modulo operator:
                     iP1 = (iEdgeOnCell + 1) % nEdgesOnCell[iCell]
                     iM1 = (iEdgeOnCell - 1) % nEdgesOnCell[iCell]
-                    # Is either of this neighbor's two neighbors to left and right ocean?
+                    # Is either of this neighbor's two neighbors to left and
+                    # right ocean?
                     # if so, sum of masks is two.
                     # subtract 1 to convert 1-base to 0-base:
-                    if (landMask[cellsOnCell[iCell, iP1]-1] == 0
-                            or landMask[cellsOnCell[iCell, iM1]-1] == 0):
+                    if (landMask[cellsOnCell[iCell, iP1] - 1] == 0
+                            or landMask[cellsOnCell[iCell, iM1] - 1] == 0):
                         landLockedCounter += 1
                         landMaskNew[iCell] = 0
                         outputMaskFile['regionCellMasks'][iCell, 1] = 0
                         landMaskDiagnostic[iCell] = 4
                         oceanMask[iCell] = 1
-                        # once we remove this cell, we can quit checking over edges
+                        # once we remove this cell, we can quit checking over
+                        # edges
                         break
 
     landMask[:] = landMaskNew[:]
     print("  Sweep: {} Number of land-locked cells returned: {}".format(
-        iSweep+1, landLockedCounter))
+        iSweep + 1, landLockedCounter))
     if landLockedCounter == 0:
         break
 
