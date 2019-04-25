8a4916730fa1aa09f282245b1364d975564dc9c2
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-04-25 13:54:00 +0200

    Move mpas_mesh_tools up a directory
    Too many scripts are needed from elsewhere in the repo

--- conda/build.sh
+++ conda/build.sh
@@ -3,16 +3,10 @@
 set -x
 set -e
 
-cp -r landice/mesh_tools_li mesh_tools/landice
-
-mkdir -p mesh_tools/ocean/
-cp -r ocean/coastline_alteration mesh_tools/ocean/coastline_alteration
-
-cd mesh_tools
 
 ${PYTHON} -m pip install . --no-deps -vv
 
-cd mesh_conversion_tools
+cd mesh_tools/mesh_conversion_tools
 
 export CXX=${GXX}
 export CFLAGS="-O3 -std=c++0x -fopenmp -lstdc++"
