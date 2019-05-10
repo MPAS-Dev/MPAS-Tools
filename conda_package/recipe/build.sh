a6a4ecfc5867a48f3a82e39e3567401104495ee8
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-05-10 07:39:14 +0200

    Add support for cmake to mesh_conversion_tools
    This is needed to be able to build on linux and OSX on conda-forge

--- conda_package/recipe/build.sh
+++ conda_package/recipe/build.sh
@@ -10,14 +10,10 @@ ${PYTHON} -m pip install . --no-deps -vv
 
 cd mesh_tools/mesh_conversion_tools
 
-export CXX=${GXX}
-export CFLAGS="-O3 -std=c++0x -fopenmp -lstdc++"
-
+# build and install JIGSAW
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
 make
-
-install -d ${PREFIX}/bin/
-for exec in MpasMeshConverter.x MpasCellCuller.x MpasMaskCreator.x
-do
-  install -m 755 ${exec} ${PREFIX}/bin/
-done
+make install
 
