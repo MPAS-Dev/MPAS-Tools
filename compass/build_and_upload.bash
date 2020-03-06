3c2d39f3a8e9c6a8480d1285e414faa6110aeac5
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-06 22:59:00 +0100

    Fix extension and update compass version
    Rename the create script to avoid confusion with e3sm-unified.

--- compass/build_and_upload.bash
+++ compass/build_and_upload.bash
@@ -2,4 +2,4 @@
 
 conda build -c conda-forge -c e3sm .
 
-#anaconda upload -u e3sm ${HOME}/miniconda3/conda-bld/noarch/compass*.tar.gz
+#anaconda upload -u e3sm ${HOME}/miniconda3/conda-bld/noarch/compass*.tar.bz2
