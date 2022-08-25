68b655cd606bc32927762908aa2d6c2123f78778
Matthew Hoffman <mhoffman@lanl.gov>
2022-08-25 11:15:16 -0700

    Add option to globalStats plotter to plot as change from initial values

--- landice/output_processing_li/plot_globalStats.py
+++ landice/output_processing_li/plot_globalStats.py
@@ -25,6 +25,7 @@ parser.add_option("-5", dest="file5inName", help="input filename", metavar="FILE
 parser.add_option("-6", dest="file6inName", help="input filename", metavar="FILENAME")
 parser.add_option("-7", dest="file7inName", help="input filename", metavar="FILENAME")
 parser.add_option("-u", dest="units", help="units for mass/volume: m3, kg, Gt", default="Gt", metavar="FILENAME")
+parser.add_option("-c", dest="plotChange", help="plot time series as change from initial.  (not applied to GL flux or calving flux)  Without this option, the full magnitude of time series is used", action='store_true')
 options, args = parser.parse_args()
 
 print("Using ice density of {} kg/m3 if required for unit conversions".format(rhoi))
@@ -49,38 +50,56 @@ print("Using volume/mass units of: ", massUnit)
 
 axVol = fig.add_subplot(nrow, ncol, 1)
 plt.xlabel('Year')
-plt.ylabel('volume ({})'.format(massUnit))
+if options.plotChange:
+   plt.ylabel('volume change ({})'.format(massUnit))
+else:
+   plt.ylabel('volume ({})'.format(massUnit))
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 axX = axVol
 
 axVAF = fig.add_subplot(nrow, ncol, 2, sharex=axX)
 plt.xlabel('Year')
-plt.ylabel('VAF ({})'.format(massUnit))
+if options.plotChange:
+   plt.ylabel('VAF change ({})'.format(massUnit))
+else:
+   plt.ylabel('VAF ({})'.format(massUnit))
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 
 axVolGround = fig.add_subplot(nrow, ncol, 3, sharex=axX)
 plt.xlabel('Year')
-plt.ylabel('grounded volume ({})'.format(massUnit))
+if options.plotChange:
+   plt.ylabel('grounded volume change ({})'.format(massUnit))
+else:
+   plt.ylabel('grounded volume ({})'.format(massUnit))
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 
 axVolFloat = fig.add_subplot(nrow, ncol, 4, sharex=axX)
 plt.xlabel('Year')
-plt.ylabel('floating volume ({})'.format(massUnit))
+if options.plotChange:
+   plt.ylabel('floating volume change ({})'.format(massUnit))
+else:
+   plt.ylabel('floating volume ({})'.format(massUnit))
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 
 axGrdArea = fig.add_subplot(nrow, ncol, 5, sharex=axX)
 plt.xlabel('Year')
-plt.ylabel('grounded area (m$^2$)')
+if options.plotChange:
+   plt.ylabel('grounded area change (m$^2$)')
+else:
+   plt.ylabel('grounded area (m$^2$)')
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 
 axFltArea = fig.add_subplot(nrow, ncol, 6, sharex=axX)
 plt.xlabel('Year')
-plt.ylabel('floating area (m$^2$)')
+if options.plotChange:
+   plt.ylabel('floating area change (m$^2$)')
+else:
+   plt.ylabel('floating area (m$^2$)')
 #plt.xticks(np.arange(22)*xtickSpacing)
 plt.grid()
 
@@ -114,6 +133,8 @@ def plotStat(fname):
        vol = vol * rhoi
     elif options.units == "Gt":
        vol = vol * rhoi / 1.0e12
+    if options.plotChange:
+        vol = vol - vol[0]
     axVol.plot(yr, vol, label=name)
 
     VAF = f.variables['volumeAboveFloatation'][:]
@@ -123,6 +144,8 @@ def plotStat(fname):
        VAF = VAF * rhoi
     elif options.units == "Gt":
        VAF = VAF * rhoi / 1.0e12
+    if options.plotChange:
+        VAF = VAF - VAF[0]
     axVAF.plot(yr, VAF, label=name)
 
     volGround = f.variables['groundedIceVolume'][:]
@@ -132,6 +155,8 @@ def plotStat(fname):
        volGround = volGround * rhoi
     elif options.units == "Gt":
        volGround = volGround * rhoi / 1.0e12
+    if options.plotChange:
+        volGround = volGround - volGround[0]
     axVolGround.plot(yr, volGround, label=name)
 
     volFloat = f.variables['floatingIceVolume'][:]
@@ -141,12 +166,18 @@ def plotStat(fname):
        volFloat = volFloat * rhoi
     elif options.units == "Gt":
        volFloat = volFloat * rhoi / 1.0e12
+    if options.plotChange:
+        volFloat = volFloat - volFloat[0]
     axVolFloat.plot(yr, volFloat, label=name)
 
     areaGrd = f.variables['groundedIceArea'][:]
+    if options.plotChange:
+        areaGrd = areaGrd - areaGrd[0]
     axGrdArea.plot(yr, areaGrd, label=name)
 
     areaFlt = f.variables['floatingIceArea'][:]
+    if options.plotChange:
+        areaFlt = areaFlt - areaFlt[0]
     axFltArea.plot(yr, areaFlt, label=name)
 
     GLflux = f.variables['groundingLineFlux'][:]
