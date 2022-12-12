8039bc20ef7c96d6a0da7a25478aef773fb790c3
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-12-12 15:18:09 +0100

    Add fix_grid_output.exe to conda package

--- conda_package/recipe/build.sh
+++ conda_package/recipe/build.sh
@@ -8,12 +8,25 @@ cp -r ocean landice visualization mesh_tools conda_package
 cd conda_package
 ${PYTHON} -m pip install . --no-deps -vv
 
-cd mesh_tools/mesh_conversion_tools
-
-# build and install JIGSAW
+# build and install sea ice partitioning tool
+cd ${SRC_DIR}/conda_package/mesh_tools/seaice_grid_tools
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
-make
-make install
+cmake \
+  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+  -D CMAKE_BUILD_TYPE=Release \
+  ..
+cmake --build .
+cmake --install .
+
+# build and install mesh conversion tools
+cd ${SRC_DIR}/conda_package/mesh_tools/mesh_conversion_tools
+mkdir build
+cd build
+cmake \
+  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+  -D CMAKE_BUILD_TYPE=Release \
+  ..
+cmake --build .
+cmake --install .
 
