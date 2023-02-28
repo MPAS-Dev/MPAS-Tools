18bfe979fe0f91898f5bf12f9cf213447aac5dca
Trevor Hillebrand <trhille@lanl.gov>
2023-02-28 15:03:45 -0800

    Fix divide-by-zero warning
    Mask thickness[prev_t,:] > 0. in face-melt processing to avoid
    divide-by-zero warning.

--- landice/output_processing_li/ismip6_postprocessing/process_flux_variables.py
+++ landice/output_processing_li/ismip6_postprocessing/process_flux_variables.py
@@ -253,7 +253,8 @@ def clean_flux_fields_before_time_averaging(file_input, file_mesh,
 
             prev_t = max(t-1, 0)  # ensure that index_cf never uses thickness from last (-1) time step
             index_cf = np.where((faceMeltingThickness[t, :] > 0.0) * (bed[:] < 0.0) *
-                                (faceMeltingThickness[t, :] != thickness[prev_t, :]))[0]
+                                (faceMeltingThickness[t, :] != thickness[prev_t, :]) * 
+                                (thickness[prev_t, :] > 0.))[0]
             for i in index_cf:
                 # faceMeltSpeed is calculated for ice below water line, but needs to be aplied
                 # to full ice thickness, so we need a vertically averaged speed. Also ensure that
