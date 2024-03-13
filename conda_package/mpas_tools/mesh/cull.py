7a05637652e9c7334b46ea8feee891b1b889856e
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-03-13 11:30:23 +0100

    Add functions for mapping from culled to base mesh

---
+++ conda_package/mpas_tools/mesh/cull.py
@@ -0,0 +1,96 @@
+import numpy as np
+import xarray as xr
+from scipy.spatial import KDTree
+
+from mpas_tools.io import write_netcdf
+
+
+def write_map_culled_to_base(base_mesh_filename, culled_mesh_filename,
+                             out_filename, workers=-1):
+    """
+    Write out a file with maps from cells, edges and vertices on a culled mesh
+    to the same elements on a base mesh.  All elements in the culled mesh must
+    be in the base mesh.
+
+    Parameters
+    ----------
+    base_mesh_filename : str
+        A file with the horizontal MPAS mesh before culling
+
+    culled_mesh_filename : str
+        A file with the culled horizonal MPAS mesh
+
+    out_filename : str
+        A file to which the maps should be written.  The dataset will include
+        ``mapCulledToBaseCell``, ``mapCulledToBaseEdge`` and
+        ``mapCulledToBaseVertex``, each of which contains the base mesh index
+        corresponding to the same element from the culled mesh.
+
+    workers : int, optional
+        The number of threads to use to query basemesh elements. The default
+        is all available threads (``workers=-1``)
+    """
+    ds_base = xr.open_dataset(base_mesh_filename)
+    ds_culled = xr.open_dataset(culled_mesh_filename)
+    ds_map_culled_to_base = map_culled_to_base(ds_base, ds_culled,
+                                               workers=workers)
+    write_netcdf(ds_map_culled_to_base, out_filename)
+
+
+def map_culled_to_base(ds_base, ds_culled, workers=-1):
+    """
+    Create maps from cells, edges and vertices on a culled mesh to the same
+    elements on a base mesh.  All elements in the culled mesh must be in the
+    base mesh.
+
+    Parameters
+    ----------
+    ds_base : xarray.Dataset
+        The horizontal MPAS mesh before culling
+
+    ds_culled : xarray.Dataset
+        The culled horizonal MPAS mesh
+
+    workers : int, optional
+        The number of threads to use to query basemesh elements. The default
+        is all available threads (``workers=-1``)
+
+    Returns
+    -------
+    ds_map_culled_to_base : xarray.Dataset
+        A dataset with ``mapCulledToBaseCell``, ``mapCulledToBaseEdge`` and
+        ``mapCulledToBaseVertex``, each of which contains the base mesh index
+        corresponding to the same element from the culled mesh.
+    """
+    ds_map_culled_to_base = xr.Dataset()
+    for dim, suffix in [('nCells', 'Cell'),
+                        ('nEdges', 'Edge'),
+                        ('nVertices', 'Vertex')]:
+        _map_culled_to_base_grid_type(ds_base, ds_culled,
+                                      ds_map_culled_to_base, dim, suffix,
+                                      workers)
+
+    return ds_map_culled_to_base
+
+
+def _map_culled_to_base_grid_type(ds_base, ds_culled, ds_map_culled_to_base,
+                                  dim, suffix, workers):
+    x_base = ds_base[f'x{suffix}'].values
+    y_base = ds_base[f'y{suffix}'].values
+    z_base = ds_base[f'z{suffix}'].values
+
+    x_culled = ds_culled[f'x{suffix}'].values
+    y_culled = ds_culled[f'y{suffix}'].values
+    z_culled = ds_culled[f'z{suffix}'].values
+
+    # create a map from lat-lon pairs to base-mesh cell indices
+    points = np.vstack((x_base, y_base, z_base)).T
+
+    tree = KDTree(points)
+
+    points = np.vstack((x_culled, y_culled, z_culled)).T
+
+    _, culled_to_base_map = tree.query(points, workers=workers)
+
+    ds_map_culled_to_base[f'mapCulledToBase{suffix}'] = \
+        ((dim,), culled_to_base_map)
