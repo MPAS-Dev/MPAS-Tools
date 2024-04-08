a9d4289f563d62a4ea3f8cfc822c0d1c385e859c
Alexander Hager <ahager@lanl.gov>
2024-04-08 16:25:54 -0600

    PR cleanup 1
    Makes minor edits to the PR in accordance with the first round of review
    that improve performance and clarity.

--- landice/mesh_tools_li/floodFillThawedIce.py
+++ landice/mesh_tools_li/floodFillThawedIce.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-
+'''
+Uses output from MALI's thermal solver to determine where regions of the bed are frozen or thawed and masks of frozen cells from the hydro domain.
+Only thawed cells in hydraulic contact with the grounding line are incorporated into the hydro domain. All other cells are set to zero ice thickness
+and are surrounded by no-flow conditions with the waterFluxMask. An additional argument, "-u", will count any frozen ice as thawed if basal sliding speeds
+exceed a threshold value (default is 25 m/yr).
+'''
 import mpas_tools
 import numpy as np
 import xarray as xr
@@ -10,26 +15,25 @@ import subprocess
 print("** Gathering information ...")
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="file", metavar="FILE")
-parser.add_option("-u", "--UbThresh", dest="UbThresh", type="float", default=25, help="basal sliding threshold used to redefined frozen ice as thawed where sliding speed is above UbThresh (in m/yr)")
+parser.add_option("-u", "--UbThresh", dest="UbThresh", type="float", default=25, help="basal sliding threshold used to redefine frozen ice as thawed where sliding speed is above UbThresh (in m/yr)")
 options, args = parser.parse_args()
 
 f = xr.open_dataset(options.file, decode_times=False, decode_cf=False)
-groundedBasalMassBal = f['groundedBasalMassBal'][0,:]
+groundedBasalMassBal = f['groundedBasalMassBal'][0,:].data
 cellsOnCell= f['cellsOnCell'][:].data
 nEdgesOnCell = f['nEdgesOnCell'][:].data
 thickness = f['thickness'][0,:].data
 bedTopography = f['bedTopography'][0,:].data
 edgesOnCell = f['edgesOnCell'][:,:].data
 yEdge = f['yEdge'][:].data
-dims = f.dims
-nVertInterfaces = dims['nVertInterfaces']
-uReconstructX = f['uReconstructX'][0,:,nVertInterfaces].data
-uReconstructY = f['uReconstructY'][0,:,nVertInterfaces].data
+nVertInterfaces = f.dims['nVertInterfaces']
+uReconstructX = f['uReconstructX'][0,:,nVertInterfaces-1].data
+uReconstructY = f['uReconstructY'][0,:,nVertInterfaces-1].data
 
 groundedMask = ((thickness*910/1028+bedTopography)>0.0)*(thickness>0.0)
-floatingMask = ((thickness*910/1028+bedTopography)<0.0)*(thickness>0.0)
+floatingMask = ((thickness*910/1028+bedTopography)<=0.0)*(thickness>0.0)
 oceanMask = (thickness==0.0)*(bedTopography<0.0)
-landMask = (thickness==0.0)*(bedTopography>0.0)
+landMask = (thickness==0.0)*(bedTopography>=0.0)
 
 groundedMask = groundedMask.reshape((len(groundedMask),1))
 floatingMask = floatingMask.reshape(len(floatingMask),1)
@@ -43,14 +47,13 @@ ind = np.where(groundedMask==1)[0]
 for i in ind:
     #identify grounded cells just inland of grounding line
     for ii in range(nEdgesOnCell[i]):
-      # if ((groundedMask[cellsOnCell[i,ii]-1] == 0):
        if ((floatingMask[cellsOnCell[i,ii]-1] == 1) | (oceanMask[cellsOnCell[i,ii]-1] == 1)):
           seedMask[i] = 1
 
 #identify grounded, thawed ice
 basalSlidingSpeed = np.sqrt(uReconstructX**2 + uReconstructY**2) * 3.15e7 #convert to m/yr
 
-growMask = (abs(groundedBasalMassBal) > 0.0)*(basalSlidingSpeed > UbThresh)
+growMask = np.logical_or(groundedBasalMassBal < 0.0, basalSlidingSpeed > options.UbThresh)
 growMask = growMask.expand_dims(dim='Time').T
 
 print("**Flood Filling ...")
@@ -62,13 +65,13 @@ print("Defining waterFluxMask ...")
 thickness[keepMask!=1] = 0
 thickness = thickness.reshape(1,len(thickness))
 
-#create zero flux mask on edges of unactive domain
+#create zero flux mask on edges of inactive domain
 waterFluxMask = np.zeros((len(yEdge),1),'int32')
 for i in range(len(yEdge)):
     cell1 = cellsOnEdge[i,0]-1
     cell2 = cellsOnEdge[i,1]-1
     if ((keepMask[cell1] == 1 and keepMask[cell2] == 0 and groundMask[cell1] == 1 and groundMask[cell2] == 1) \
-        or (keepMask[cell2] == 1 and keepMask[cell1] == 0 and groundMask[cell1] and groundMask[cell2])):
+        or (keepMask[cell2] == 1 and keepMask[cell1] == 0 and groundMask[cell1] == 1 and groundMask[cell2] == 1)):
         waterFluxMask[i] = 2
 
 print("Saving ....")
@@ -77,8 +80,8 @@ f = f.drop_vars(['thickness'])
 f = f.drop_vars(['xtime'])
 f = f.drop_vars(['simulationStartTime'])
 
-basalMeltInput = abs(groundedBasalMassBal)
-bmi = xr.DataArray(basalMeltInput.T.astype('int32'),dims=('Time','nEdges')
+basalMeltInput = -1.0 * np.minimum(0.0, groundedBasalMassBal)
+bmi = xr.DataArray(basalMeltInput.T.astype('int32'),dims=('Time','nCells'))
 f['basalMeltInput'] = bmi
 
 wfm = xr.DataArray(waterFluxMask.T.astype('int32'),dims=('Time','nEdges'))
@@ -88,8 +91,8 @@ thk = xr.DataArray(thickness.astype('float64'),dims=('Time','nCells'))
 f['thickness'] = thk
 
 #Remove fill values automatically added by xarray
-for varname in f.variables
-    if '_FillValue' in f.variables[varname].attrs
+for varname in f.variables:
+    if '_FillValue' in f.variables[varname].attrs:
         del f.variables.attrs['_FillValue']
 
 f.to_netcdf("finalMaskedFile.nc")
