29e9c2a1221188f13b80218c4258b97b3ae65525
Trevor Hillebrand <trhille@lanl.gov>
2024-07-09 11:03:34 -0700

    Clean up for PEP8 compliance

--- landice/mesh_tools_li/extrapolate_variable.py
+++ landice/mesh_tools_li/extrapolate_variable.py
@@ -16,12 +16,18 @@ import sys
 from datetime import datetime
 
 
-parser = OptionParser(description='Convert data from exodus file to MPAS mesh. WARNING: Change the SEACAS library dir to your own path! A simple usage example: conversion_exodus_init_to_mpasli_mesh.py -e antarctica.exo -o target.nc -a ./mpas_cellID.ascii -v beta')
+parser = OptionParser(
+    description='Extrapolate a variable into undefined regions')
 
-parser.add_option("-f", "--file", dest="nc_file", help="the mpas file to write to")
-parser.add_option("-v", "--variable", dest="var_name", help="the mpas variable(s) you want to extrapolate")
-parser.add_option("-m", "--method", dest="extrapolation", default='min', help="idw, min, or value method of extrapolation")
-parser.add_option("-s", "--set_value", dest="set_value", default=None, help="value to set variable to outside keepCellMask when using -v value")
+parser.add_option("-f", "--file", dest="nc_file",
+                  help="the mpas file to write to")
+parser.add_option("-v", "--variable", dest="var_name",
+                  help="the mpas variable(s) you want to extrapolate")
+parser.add_option("-m", "--method", dest="extrapolation", default='min',
+                  help="idw, min, or value method of extrapolation")
+parser.add_option("-s", "--set_value", dest="set_value", default=None,
+                  help=("value to set variable to outside "
+                        "keepCellMask when using -v value"))
 
 for option in parser.option_list:
     if option.default != ("NO", "DEFAULT"):
@@ -51,7 +57,8 @@ elif dataset.variables[var_name].ndim == 3:
     print(dataset.variables[var_name].dimensions[2])
     vert_dim_name = dataset.variables[var_name].dimensions[2]
     n_vert = len(dataset.dimensions[vert_dim_name])
-    print(f"This variable has a vertical dimension of {vert_dim_name} with size {n_vert}")
+    print(f"This variable has a vertical dimension "
+          f"of {vert_dim_name} with size {n_vert}")
     print("")
 else:
     print("Unexpected number of dimension in variable")
@@ -63,7 +70,8 @@ for v in range(n_vert):
     else:
         varValue = dataset.variables[var_name][0, :]
 
-    # Define region of good data to extrapolate from.  Different methods for different variables
+    # Define region of good data to extrapolate from.
+    # Different methods for different variables
     if var_name in ["effectivePressure", "beta", "muFriction"]:
         groundedMask = (thickness > (-1028.0 / 910.0 * bed))
         keepCellMask = np.copy(groundedMask) * np.isfinite(varValue)
@@ -75,8 +83,10 @@ for v in range(n_vert):
                 if (groundedMask[jCell] == 1):
                     keepCellMask[iCell] = 1
                     continue
-        keepCellMask *= (varValue > 0)  # ensure zero muFriction does not get extrapolated
-        keepCellMask *= (varValue < 1.e12)  # get rid of ridiculous values
+        # ensure zero muFriction does not get extrapolated
+        keepCellMask *= (varValue > 0)
+        # Get rid of invalid values
+        keepCellMask *= (varValue < 1.e12)
     elif var_name in ["floatingBasalMassBal"]:
         floatingMask = (thickness <= (-1028.0 / 910.0 * bed))
         keepCellMask = floatingMask * (varValue != 0.0)
@@ -84,13 +94,17 @@ for v in range(n_vert):
     else:
         keepCellMask = (thickness > 0.0)
 
-    keepCellMaskNew = np.copy(keepCellMask)  # make a copy to edit that will be used later
-    keepCellMaskOrig = np.copy(keepCellMask)  # make a copy to edit that can be edited without changing the original
+    # make a copy to edit that will be used later
+    keepCellMaskNew = np.copy(keepCellMask)
+    # make a copy to edit that can be edited without changing the original
+    keepCellMaskOrig = np.copy(keepCellMask)
 
     # recursive extrapolation steps:
     # 1) find cell A with mask = 0
-    # 2) find how many surrounding cells have nonzero mask, and their indices (this will give us the cells from upstream)
-    # 3) use the values for nonzero mask upstream cells to extrapolate the value for cell A
+    # 2) find how many surrounding cells have nonzero mask, and
+    #    their indices (this will give us the cells from upstream)
+    # 3) use the values for nonzero mask upstream
+    #    cells toextrapolate the value for cell A
     # 4) change the mask for A from 0 to 1
     # 5) Update mask
     # 6) go to step 1)
@@ -106,12 +120,14 @@ for v in range(n_vert):
 
             for iCell in searchCells:
                 neighborcellID = cellsOnCell[iCell,:nEdgesOnCell[iCell]]-1
-                neighborcellID = neighborcellID[neighborcellID>=0] # Important: ignore the phantom "neighbors" that are off the edge of the mesh (0 values in cellsOnCell)
-
-                mask_for_idx = keepCellMask[neighborcellID] # active cell mask
+                # Important: ignore the phantom "neighbors" that are
+                # off the edge of the mesh (0 values in cellsOnCell)
+                neighborcellID = neighborcellID[neighborcellID>=0]
+                # active cell mask
+                mask_for_idx = keepCellMask[neighborcellID]
                 mask_nonzero_idx, = np.nonzero(mask_for_idx)
-
-                nonzero_id = neighborcellID[mask_nonzero_idx] # id for nonzero beta cells
+                # id for nonzero beta cells
+                nonzero_id = neighborcellID[mask_nonzero_idx]
                 nonzero_num = np.count_nonzero(mask_for_idx)
 
                 assert len(nonzero_id) == nonzero_num
@@ -130,26 +146,31 @@ for v in range(n_vert):
                     elif extrapolation == 'min':
                         varValue[iCell] = np.min(var_adj)
                     else:
-                        sys.exit("ERROR: wrong extrapolation scheme! Set option m as idw or min!")
+                        sys.exit("ERROR: wrong extrapolation scheme!"
+                                 " Set option m as idw or min!")
 
                     keepCellMaskNew[iCell] = 1
 
-            print ("{0:8d} cells left for extrapolation in total {1:8d} cells".format(nCells-np.count_nonzero(keepCellMask),  nCells))
+            print(f"{nCells-np.count_nonzero(keepCellMask)} cells left "
+                  f"for extrapolation in total {nCells} cells")
 
+    # Write updated array to file
     if has_vertical_dim == True:
-        dataset.variables[var_name][0,:,v] = varValue # Put updated array back into file.
+        dataset.variables[var_name][0,:,v] = varValue
     else:
-        dataset.variables[var_name][0,:] = varValue # Put updated array back into file.
+        dataset.variables[var_name][0,:] = varValue
+
 # === Clean-up =============
 
 # Update history attribute of netCDF file
-thiscommand = datetime.now().strftime("%a %b %d %H:%M:%S %Y") + ": " + " ".join(sys.argv[:])
-thiscommand = thiscommand+";  {} successfully extrapolated using the {} method".format(var_name, extrapolation)
+thiscommand = datetime.now().strftime("%a %b %d %H:%M:%S %Y") \
+              + ": " + " ".join(sys.argv[:])
+thiscommand = thiscommand + f";  {var_name} successfully extrapolated " + \
+              f"using the {extrapolation} method"
 if hasattr(dataset, 'history'):
    newhist = '\n'.join([thiscommand, getattr(dataset, 'history')])
 else:
    newhist = thiscommand
 setattr(dataset, 'history', newhist)
-
 
 dataset.close()
