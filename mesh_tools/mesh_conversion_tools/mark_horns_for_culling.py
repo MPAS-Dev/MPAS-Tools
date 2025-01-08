4aa479d2432445a6f502a09789d2e5c29b27e53f
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-01-08 06:29:11 -0600

    Move `mark_horns_for_culling` into conda package
    Clean up a little bit for PEP8.
    
    Leave a stub in the location of the original script that calls
    the entry point in the conda package.

--- mesh_tools/mesh_conversion_tools/mark_horns_for_culling.py
+++ mesh_tools/mesh_conversion_tools/mark_horns_for_culling.py
@@ -10,72 +10,8 @@ It adds the new masked cell to an existing 'cullCell' field if it exists,
 otherwise it creates a new field.
 '''
 
-from __future__ import absolute_import, division, print_function, \
-    unicode_literals
-
-import sys
-import numpy as np
-import netCDF4
-from optparse import OptionParser
-from datetime import datetime
+from mpas_tools.mesh.mark_horns_for_culling import main
 
 
-print("== Gathering information.  (Invoke with --help for more details. All "
-      "arguments are optional)\n")
-parser = OptionParser()
-parser.description = __doc__
-parser.add_option(
-    "-f",
-    "--file",
-    dest="inputFile",
-    help="Name of file to be processed.",
-    default="grid.nc",
-    metavar="FILENAME")
-for option in parser.option_list:
-    if option.default != ("NO", "DEFAULT"):
-        option.help += (" " if option.help else "") + "[default: %default]"
-options, args = parser.parse_args()
-
-print("  File to be modified:  " + options.inputFile)
-
-
-# Open file and get needed fields.
-inputFile = netCDF4.Dataset(options.inputFile, 'r+')
-nCells = len(inputFile.dimensions['nCells'])
-cellsOnCell = inputFile.variables['cellsOnCell'][:]
-
-# Add the horn cells to existing mask if it exists
-if 'cullCell' in inputFile.variables:
-    cullCell = inputFile.variables['cullCell'][:]
-else:  # otherwise make a new mask initialized empty
-    cullCell = np.zeros((nCells,))  # local variable
-
-nHorns = 0
-for i in range(nCells):
-    # NOTE: Can change this threshold, if needed for a particular use case.
-    if (cellsOnCell[i, :] > 0).sum() <= 2:
-        cullCell[i] = 1
-        nHorns += 1
-
-# Write out the new field
-if 'cullCell' in inputFile.variables:
-    cullCellVar = inputFile.variables['cullCell']
-else:
-    cullCellVar = inputFile.createVariable('cullCell', 'i', ('nCells',))
-cullCellVar[:] = cullCell
-
-
-# Update history attribute of netCDF file
-thiscommand = datetime.now().strftime("%a %b %d %H:%M:%S %Y") + \
-    ": " + " ".join(sys.argv[:])
-if hasattr(inputFile, 'history'):
-    newhist = '\n'.join([thiscommand, getattr(inputFile, 'history')])
-else:
-    newhist = thiscommand
-setattr(inputFile, 'history', newhist)
-
-inputFile.close()
-
-print('\n{} "horn" locations have been marked in the field cullCell.'.format(
-    nHorns))
-print("Remember to use MpasCellCuller.x to actually remove them!")
+if __name__ == '__main__':
+    main()
