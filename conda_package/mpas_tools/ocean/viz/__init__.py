da7d3ba551e8c80d77ddb1af5c67c9e12bf58179
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-02-09 11:44:20 +0100

    Port transect support from Polaris
    The primary change is that transects are plotted using quads,
    rather than triangles.  This has various advantages:
    
    * quads are more intuitive
    * we can plot contours with labels
    * the `matplotlib` functionality for plotting fields on quads
      is a lot more robust, mature, and intuitive than that for triangles
    
    This merge also adds some missing documentation on public
    transect-related functions.

--- conda_package/mpas_tools/ocean/viz/__init__.py
+++ conda_package/mpas_tools/ocean/viz/__init__.py
@@ -1,2 +1,1 @@
-from mpas_tools.ocean.viz.transects import plot_ocean_transects
-from mpas_tools.ocean.viz.inset import add_inset
+from mpas_tools.ocean.viz.inset import add_inset  # noqa: F401
---
+++ conda_package/mpas_tools/viz/transect/__init__.py
@@ -0,0 +1,7 @@
+from mpas_tools.viz.transect.horiz import (  # noqa: F401
+    find_planar_transect_cells_and_weights,
+    find_spherical_transect_cells_and_weights,
+    interp_mpas_horiz_to_transect_nodes,
+    make_triangle_tree,
+    mesh_to_triangles,
+)
