10fdec806712b1940ac4414f22a16bfab9040101
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-03-31 12:20:02 -0500

    Remove stup scripts for making SCRIP files
    Folks can use the conda package.

--- mesh_tools/create_SCRIP_files/create_SCRIP_file_from_planar_rectangular_grid.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# Create a SCRIP file from a planar rectanfular mesh.
-# See for details: http://www.earthsystemmodeling.org/esmf_releases/public/ESMF_5_2_0rp1/ESMF_refdoc/node3.html#SECTION03024000000000000000
-
-from mpas_tools.scrip.from_planar import main
-
-
-if __name__ == '__main__':
-    main()
