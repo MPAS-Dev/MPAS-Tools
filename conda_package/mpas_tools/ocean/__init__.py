bacc904cdb086c67a35fbc15885a834c38ac7d51
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-06-14 08:25:27 -0500

    Improve performance of barotropic streamfunction
    This merge refactors the barotropic streamfunction, separating
    out computation of vertically integrated velocity and vorticity
    into their own modules.  It improves performance and memory usage
    for large meshes by:
    * chunking along the `nEdges` dimension during computation of the
      vertically integrated velocity
    * saving to intermediate files if a temporary directory is provided
    * loading numpy array for subsequent (2D) computations

--- conda_package/mpas_tools/ocean/__init__.py
+++ conda_package/mpas_tools/ocean/__init__.py
@@ -1,16 +1,24 @@
 from mpas_tools.ocean.build_mesh import (
-    build_spherical_mesh,
-    build_planar_mesh,
+    build_planar_mesh as build_planar_mesh,
 )
-from mpas_tools.ocean.barotropic_streamfunction import (
-    compute_barotropic_streamfunction,
+from mpas_tools.ocean.build_mesh import (
+    build_spherical_mesh as build_spherical_mesh,
 )
-from mpas_tools.ocean.inject_bathymetry import inject_bathymetry
+from mpas_tools.ocean.inject_bathymetry import (
+    inject_bathymetry as inject_bathymetry,
+)
 from mpas_tools.ocean.inject_meshDensity import (
-    inject_meshDensity_from_file,
-    inject_spherical_meshDensity,
-    inject_planar_meshDensity,
+    inject_meshDensity_from_file as inject_meshDensity_from_file,
+)
+from mpas_tools.ocean.inject_meshDensity import (
+    inject_planar_meshDensity as inject_planar_meshDensity,
+)
+from mpas_tools.ocean.inject_meshDensity import (
+    inject_spherical_meshDensity as inject_spherical_meshDensity,
 )
 from mpas_tools.ocean.inject_preserve_floodplain import (
-    inject_preserve_floodplain,
+    inject_preserve_floodplain as inject_preserve_floodplain,
+)
+from mpas_tools.ocean.streamfunction.barotropic import (
+    compute_barotropic_streamfunction as compute_barotropic_streamfunction,
 )
