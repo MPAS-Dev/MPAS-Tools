597cdf1cffaa99d9d9d67969ed16e43318f292e2
Matthew Hoffman <mhoffman@lanl.gov>
2026-02-27 07:40:10 -0700

    Revisions from code review
    Enhance spherical coordinate conversion script:
    * don't overwrite input file
    * clean up to comments and docstring
    * addition of documentation section
    * error message when required fields are missing

--- landice/mesh_tools_li/convert_cell_coordinates_to_sphere.py
+++ landice/mesh_tools_li/convert_cell_coordinates_to_sphere.py
@@ -4,15 +4,15 @@ Script to allow plotting MALI output on a sphere in Paraview.
 This works by replacing x/y/zCell fields and modifying global attributes so
 Paraview will plot as a spherical mesh.  Paraview only uses those 3 fields for
 plotting.
-The script modifies the file in place, so be sure to make a copy first!
-If you screw that up, you can always copy the planar cell coordinate fields
-from another file and set on_a_sphere back to NO.
+The script writes to a separate output file and leaves the input file unchanged.
 Note that the modified coordinates will make the file no longer compatible
 with MALI or many postprocessing tools, so this is meant as a modification
-for visuatlization in Paraview only.
+for visualization in Paraview only.
 '''
 
 import argparse
+import shutil
+from pathlib import Path
 from netCDF4 import Dataset
 import numpy as np
 
@@ -20,8 +20,10 @@ parser = argparse.ArgumentParser(
     prog='convert_cell_coordinates_to_sphere.py',
     description=__doc__,
     formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument("-f", dest="filename", default="output.nc",
-                    help="file to convert to a spherical mesh convention")
+parser.add_argument("-i", "--infile", dest="infile", default="output.nc",
+                    help="input file to convert to spherical mesh convention")
+parser.add_argument("-o", "--outfile", dest="outfile", default=None,
+                    help="output file to write (default: <infile stem>_sphere<suffix>)")
 parser.add_argument("-r", dest="radius", type=float, default=6371220.,
                     help="sphere radius to use (m)")
 parser.add_argument("-w", dest="warp_surface", action='store_true',
@@ -34,6 +36,17 @@ parser.add_argument("-w", dest="warp_surface", action='store_true',
 parser.add_argument("-s", dest="scale", type=float, default=100.,
                     help="scale factor for warping surface")
 args = parser.parse_args()
+
+infile = Path(args.infile)
+if args.outfile is None:
+    outfile = infile.with_name(f'{infile.stem}_sphere{infile.suffix}')
+else:
+    outfile = Path(args.outfile)
+
+if infile.resolve() == outfile.resolve():
+    raise ValueError('Output file must be different from input file.')
+
+shutil.copyfile(infile, outfile)
 
 
 def compute_xyz(latCell_deg, lonCell_deg, radius):
@@ -50,29 +63,36 @@ def compute_xyz(latCell_deg, lonCell_deg, radius):
     return xCell, yCell, zCell
 
 
-ds = Dataset(args.filename, "r+")
-latCell = np.degrees(ds.variables["latCell"][:])
-lonCell = np.degrees(ds.variables["lonCell"][:])
+with Dataset(outfile, "r+") as ds:
+    latCell = np.degrees(ds.variables["latCell"][:])
+    lonCell = np.degrees(ds.variables["lonCell"][:])
 
-if args.warp_surface:
-    if 'upperSurface' in ds.variables:
-        sfc = ds.variables['upperSurface'][0,:]
+    if args.warp_surface:
+        has_upper_surface = 'upperSurface' in ds.variables
+        has_thickness = 'thickness' in ds.variables
+        has_bed_topography = 'bedTopography' in ds.variables
+
+        if not (has_upper_surface or (has_thickness and has_bed_topography)):
+            raise ValueError(
+                "Cannot warp surface: provide 'upperSurface' or both "
+                "'thickness' and 'bedTopography'.")
+
+        if has_upper_surface:
+            sfc = ds.variables['upperSurface'][0,:]
+        else:
+            thickness = ds.variables['thickness'][0,:]
+            bedTopography = ds.variables['bedTopography'][0,:]
+            sfc = np.maximum(bedTopography + thickness,
+                             (1.0 - 910.0 / 1028.0) * thickness)
+        radius = args.radius + np.maximum(sfc, 0.0) * args.scale
     else:
-        thickness = ds.variables['thickness'][0,:]
-        bedTopography = ds.variables['bedTopography'][0,:]
-        sfc = np.maximum(bedTopography + thickness,
-                         (1.0 - 910.0 / 1028.0) * thickness)
-        #sfc = thickness + np.minimum(bedTopography, 0.0) * 1028.0 / 910.0
-    radius = args.radius + np.maximum(sfc, 0.0) * args.scale
-else:
-    radius = args.radius
-xCell, yCell, zCell = compute_xyz(latCell, lonCell, radius)
+        radius = args.radius
+    xCell, yCell, zCell = compute_xyz(latCell, lonCell, radius)
 
-# Add to NetCDF file (assumes variables already exist)
-ds.variables["xCell"][:] = xCell
-ds.variables["yCell"][:] = yCell
-ds.variables["zCell"][:] = zCell
-# Update global attributes
-ds.setncattr("on_a_sphere", "YES")
-ds.setncattr("sphere_radius", args.radius)
-ds.close()
+    # Add to NetCDF file (assumes variables already exist)
+    ds.variables["xCell"][:] = xCell
+    ds.variables["yCell"][:] = yCell
+    ds.variables["zCell"][:] = zCell
+    # Update global attributes
+    ds.setncattr("on_a_sphere", "YES")
+    ds.setncattr("sphere_radius", args.radius)
