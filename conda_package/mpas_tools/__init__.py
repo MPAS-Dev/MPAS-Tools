2582e4b9439746f049f6a4b84abeac41981c1cd9
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-04-03 14:08:59 +0200

    Update to v1.5.0rc1

--- conda_package/mpas_tools/__init__.py
+++ conda_package/mpas_tools/__init__.py
@@ -1,2 +1,3 @@
-__version_info__ = (1, 4, 0)
-__version__ = '.'.join(str(vi) for vi in __version_info__)
+__version_info__ = (1, 5, 0)
+# __version__ = '.'.join(str(vi) for vi in __version_info__)
+__version__ = '1.5.0rc1'
